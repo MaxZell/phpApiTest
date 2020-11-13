8b57ffc5e3b6eeca8577e55168cd9ef0c7f82ec3
mzelensky <max.m.zelensky@gmail.com>
2020-11-13 08:40:05 +0100

    index4

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 <body>
     <?php 
         $currentYear = date("Y");
-        $currentM = date("m");
+        // $currentM = date("m");
+        $currentM = "January";
         $t=date('d-m-Y');
         $currentD = strtolower(date("d",strtotime($t)));
         error_log("t " . $t);
@@ -16,9 +17,8 @@
         error_log("month " . $currentM);
         error_log("day " . $currentD);
 
-        // //month array from current month, not from January
-        $monthsOfYear = array();//change
-        // $monthsOfYear = array('zero');//change
+        //month array from current month, not from January
+        $monthsOfYear = array();
         for ($i = 0; $i < 12; $i++) {
             $timestamp = mktime(0, 0, 0, date('n') + $i, 1);
             array_push($monthsOfYear, date('F', $timestamp));
@@ -33,8 +33,14 @@
 
         $yearTable = "<table class='myTable'>";
         $spanOld = 0;
+        $withoutStart = false;
+               
         for($month = 1; $month<=12; $month++){
             //month dates
+            $rightMonth = $monthsOfYearAsNum[$month];
+            if($rightMonth==1 And $currentM != 1){
+                $currentYear++;
+            }
             $numberOfdaysInMonth = cal_days_in_month(CAL_GREGORIAN, $monthsOfYearAsNum[$month], $currentYear);
             $yearTable .= "<tr><th class='month'>" . ($monthsOfYear[(int)$month] . "</th>");
             for($day=1; $day<=$numberOfdaysInMonth; $day++){
@@ -42,65 +48,70 @@
             }
             //CW
             $yearTable .= "</tr><tr><th class='title-col'>CW</th>";
-            $daysToMo = false;
-            $volleWochen = 0;
+            $percentTable = "";
+            $fullWeeks = 0;
             $startDat = 0;
-            
-            $spanGemacht = false;
+            $spanDone = false;
+            $counter = 0;
+            if($rightMonth == $currentM){
+                for($day=1; $day<=$numberOfdaysInMonth; $day++){  
+                    $rightMonth = $monthsOfYearAsNum[$month];
+                    $weekName = date("l", mktime(0,0,0,$rightMonth,$day,$currentYear));
+                    if($weekName != "Monday"){
+                        $counter ++;
+                    }else{
+                        break;
+                    }
+                }
+            }
+            $spanOld = $spanOld +$counter;
             for($day=1; $day<=$numberOfdaysInMonth; $day++){  
                 $rightMonth = $monthsOfYearAsNum[$month];
-                
                 $weekName = date("l", mktime(0,0,0,$rightMonth,$day,$currentYear));
-                
-                if($daysToMo == false And $weekName != "Monday" And $spanGemacht == false){
-                    if ($spanOld != 0){
-                        $spanGemacht = true;
-                        $startDat ++;
-                        $span = 7 - $spanOld;
-                        $yearTable .= "<th class='kw' colspan='$span'>" . (int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear)) ."</th>";
-                        $volleWochen = floor(($numberOfdaysInMonth - $startDat) / 7);
-                    }
-                    else{
-                        $spanGemacht = true;
-                        $startDat ++;
-                        $yearTable .= "<th class='kw'>" . (int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear)) ."</th>";
-                        $volleWochen = floor(($numberOfdaysInMonth - $startDat) / 7);
-                    }
 
-                    
+                if($withoutStart == true And $day == 1 And $weekName == "Monday"){
+                    $fullWeeks = floor($numberOfdaysInMonth / 7);
+                    $withoutStart = false;
+                    $spanDone = true;
                 }
-                else{
-                    
-                    if($weekName=="Monday" And $volleWochen != 0){
-                        $volleWochen--;
-                        
-                        $daysToMo = true;
-                        $yearTable .= "<th class='kw' colspan= '7'>" . (int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear)) . "</th>";//hour, minute, second, month, day, year
+                if($weekName != "Monday" And $spanDone == false){
+                    if ($spanOld != 0 And $counter == 0){
+                        $spanDone = true;
+                        $span = 7 - $spanOld;
+                        $startDat = $span;
+                        $yearTable .= "<th class='kw' colspan='$span'>" . (int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear)) ."</th>";
+                        $percentTable .= "<th id='noData' colspan='$span' class='".(int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear))."'></th>";
+                        $fullWeeks = floor(($numberOfdaysInMonth - $startDat) / 7);
+                    }else{   
+                        $spanDone = true;
+                        $span = $spanOld;
+                        $startDat = $span;
+                        $yearTable .= "<th class='kw' colspan='$span'>" . (int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear)) ."</th>";
+                        $percentTable .= "<th id='noData' colspan='$span' class='".(int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear))."'></th>";
+                        $fullWeeks = floor(($numberOfdaysInMonth - $startDat) / 7);
                     }
-                    else if ($volleWochen == 0 And $weekName=="Monday"){
-                        $yearTable .= "<th class='kw' colspan= '". strval($numberOfdaysInMonth - $day + 1) . "'>" . (int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear)) ."</th>";
-                        echo "<p>" . strval($numberOfdaysInMonth - $day + 1) . "</p>"; 
+                }else{
+                    if($weekName=="Monday" && $fullWeeks != 0){   
+                        $fullWeeks--;
+                        $yearTable .= "<th class='kw' colspan= '7'>" . (int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear)) . "</th>";//hour, minute, second, month, day, year
+                        $percentTable .= "<th id='noData' colspan='7' class='".(int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear))."'></th>";
                         $spanOld = $numberOfdaysInMonth - $day + 1;
-                    break;
+                        if($spanOld == 7){
+                            $withoutStart = true;
+                        }
+                    }elseif ($fullWeeks == 0 && $weekName=="Monday"){
+                        $yearTable .= "<th class='kw' colspan= '". strval($numberOfdaysInMonth - $day + 1) . "'>" . (int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear)) ."</th>";
+                        $percentTable .= "<th id='noData' colspan='". strval($numberOfdaysInMonth - $day + 1) . "' class='".(int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear))."'></th>";
+                        $spanOld = $numberOfdaysInMonth - $day + 1;
+                        break;
                     }
                 }  
             }
             //procent_id
             $yearTable .= "</tr><tr><th class='title-col'>Procent</th>";
-            $wday=1;
-            //first week days problem
-            for($day=1; $day<=$numberOfdaysInMonth; $day++){
-                (string)$dateString = (string)$currentYear . "-" . (string)sprintf("%02d", $monthsOfYearAsNum[$month]) . "-" . (string)sprintf("%02d", $day);
-                $weekDay = date("l", strtotime($dateString));
-                if($weekDay == "Monday"){
-                    $yearTable .= "<th class='noData' colspan='7'".$wday."' id='".(int)date("W", mktime(0,0,0,$rightMonth,$day,$currentYear))."'>NoData</th>";
-                    $wday=0;
-                }
-                $wday++;
-            }
+            $yearTable .= $percentTable;
         }
         $yearTable .= "</tr></table>";
-
         echo $yearTable; 
     ?>
     <script>
@@ -109,8 +120,16 @@
         xhttp.onload = () => {
             let response = xhttp.response;
             let jsonObj = JSON.parse(response);
-            jsonObj.forEach(element => console.log(element.WorkWeek, element.PlannedPercent))
-            jsonObj.forEach(element => document.getElementById(element.WorkWeek).innerHTML = element.PlannedPercent)
+            jsonObj.forEach((date) =>{
+                console.log(date.WorkWeek, date.PlannedPercent);
+                jsonObj.forEach(cw => {
+                    var divs = document.getElementsByClassName(cw.WorkWeek);
+                    [].slice.call(divs).forEach(function (div) {
+                        div.innerHTML = cw.PlannedPercent;
+                        div.id = 'monday-'+ cw.PlannedPercent;
+                    });
+                });
+            });
         }
 		xhttp.send();
     </script>
@@ -119,42 +138,43 @@
         div.username{background: green; size: 300px; border: none; font-size: 20px; color: white; padding: 5px 30px; width: min-content; position: relative; left: 0px; margin: 2px;}
         button.close{ background: #f44336; border: none; font-size: 20px; border-radius: 10px; color: white; padding: 10px 30px; cursor: pointer;}
         /*th, td {border: 1px solid black;}*/
-        th, td {min-width: 20px;}
+        th, td {min-width: 25px;}
+        /* table{table-layout: fixed;width: 100%;} */
         /*https://colorhunt.co/palette/180289*/
         th.noBorder {border: 0px solid black;}
         th{font-family: sans-serif; font-size: 17px; height: 15px; background: #AB6FE3;}
         th.withBorder{border: 2px solid black;}
-        th.mday{background: #0090ee; color: wheat; text-align: center;}/*th.mday{background: darkgray;}*/
+        th.mday{background: #0090ee; color: wheat; text-align: center; max-width: 20px;}/*th.mday{background: darkgray;}*/
         th.wname{background: #00909e; color: whitesmoke; text-align: center;}/*th.wname{background: gray;}*/
         th.kw{background: lightblue; text-align: center;}
         th.month{background: #142850; color: wheat; padding: 5px;} /*th.month{background: #BF8888; padding: 0px;}*/
         th.title-col{background: #27496d; color: #dae1e7;}/*th.title-col{background: silver;}*/
-        th.monday-10{background: darkred;}
-        th.monday-10-2{background: #B40000;}
-        th.monday-20{background: red;}
-        th.monday-20-2{background: #FF6666;}
-        th.monday-30{background: darkorange;}
-        th.monday-30-2{background: #FFA83D;}
-        th.monday-30{background: orange;}
-        th.monday-30-2{background: #FFB429;}
-        th.monday-40{background: #d1b610;}
-        th.monday-40-2{background: #D1C784;}
-        th.monday-50{background: #FFFFE0;}
-        th.monday-50-2{background: #FFFFE6;}
-        th.monday-60{background: #FFFFE0;}
-        th.monday-60-2{background: #FFFFE6;}
-        th.monday-70{background: greenyellow;}
-        th.monday-70-2{background: #CBFF7D;}
-        th.monday-80{background: greenyellow;}
-        th.monday-80-2{background: #CBFF7D;}
-        th.monday-90{background: greenyellow;}
-        th.monday-90-2{background: #CBFF7D;}
-        th.monday-100{background: green;}
-        th.monday-100-2{background: #6A806A;}
-        th.monday-120{background: #006400;}
-        th.monday-120-2{background: #356654;}
-        th.noData{background: blueviolet;}
-        th.noData-2{background: #AB6FE3;}
+        th#monday-10{background: darkred;}
+        th#monday-10-2{background: #B40000;}
+        th#monday-20{background: red;}
+        th#monday-20-2{background: #FF6666;}
+        th#monday-30{background: darkorange;}
+        th#monday-30-2{background: #FFA83D;}
+        th#monday-30{background: orange;}
+        th#monday-30-2{background: #FFB429;}
+        th#monday-40{background: #d1b610;}
+        th#monday-40-2{background: #D1C784;}
+        th#monday-50{background: #FFFFE0;}
+        th#monday-50-2{background: #FFFFE6;}
+        th#monday-60{background: #FFFFE0;}
+        th#monday-60-2{background: #FFFFE6;}
+        th#monday-70{background: greenyellow;}
+        th#monday-70-2{background: #CBFF7D;}
+        th#monday-80{background: greenyellow;}
+        th#monday-80-2{background: #CBFF7D;}
+        th#monday-90{background: greenyellow;}
+        th#monday-90-2{background: #CBFF7D;}
+        th#monday-100{background: green;}
+        th#monday-100-2{background: #6A806A;}
+        th#monday-120{background: #006400;}
+        th#monday-120-2{background: #356654;}
+        th#noData{background: blueviolet;}
+        th#noData-2{background: #AB6FE3;}
         /*th.monday{background: #f44336; padding: 0px;}*/
     </style>
 </body>
